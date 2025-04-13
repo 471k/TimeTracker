f90b1314b559667a2cb55165cafe7d1b8495e089
alik <42393919+471k@users.noreply.github.com>
2025-04-13 22:42:38 +0200

    Implemented Registration functionality and added blazored Toast for displaying errors

--- TimeTracker.Client/Program.cs
+++ TimeTracker.Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TimeTracker.Client;
@@ -10,5 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddBlazoredToast();
+
 
 await builder.Build().RunAsync();
