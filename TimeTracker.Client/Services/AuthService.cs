ee51d98392448b535c3246aa44d150a28160dfa6
alik <42393919+471k@users.noreply.github.com>
2025-04-14 14:29:49 +0200

    Implemented Authorizatoin functionalities

--- TimeTracker.Client/Services/AuthService.cs
+++ TimeTracker.Client/Services/AuthService.cs
@@ -1,6 +1,10 @@
-﻿using Blazored.Toast.Services;
+﻿using Blazored.LocalStorage;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Json;
 using TimeTracker.Shared.Models.Account;
+using TimeTracker.Shared.Models.Login;
 
 namespace TimeTracker.Client.Services
 {
@@ -8,11 +12,56 @@ namespace TimeTracker.Client.Services
     {
         private readonly HttpClient _http;
         private readonly IToastService _toastService;
+        private readonly NavigationManager _navigationManager;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationStateProvider _authStateProvider;
 
-        public AuthService(HttpClient http, IToastService toastService)
+        public AuthService(
+            HttpClient http,
+            IToastService toastService,
+            NavigationManager navigationManager,
+            ILocalStorageService localStorage,
+            AuthenticationStateProvider authStateProvider)
         {
             _http = http;
             _toastService = toastService;
+            _navigationManager = navigationManager;
+            _localStorage = localStorage;
+            _authStateProvider = authStateProvider;
+        }
+
+        public async Task Login(LoginRequest request)
+        {
+            var result = await _http.PostAsJsonAsync("api/login", request);
+            if(result != null)
+            {
+                var response = await result.Content.ReadFromJsonAsync<LoginResponse>();
+                if(!response.IsSuccessful && response.Error != null)
+                {
+                    _toastService.ShowError(response.Error);
+                }
+                else if(!response.IsSuccessful)
+                {
+                    _toastService.ShowError("An unexpected error occurred.");
+                }
+                else
+                {
+                    if (response.Token != null)
+                    {
+                        await _localStorage.SetItemAsStringAsync("authToken", response.Token);
+                        await _authStateProvider.GetAuthenticationStateAsync();
+                    }
+                    _navigationManager.NavigateTo("timeentries");
+                    
+                }
+            }
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            await _authStateProvider.GetAuthenticationStateAsync();
+            _navigationManager.NavigateTo("/login");
         }
 
         public async Task Register(AccountRegistrationRequest request)
